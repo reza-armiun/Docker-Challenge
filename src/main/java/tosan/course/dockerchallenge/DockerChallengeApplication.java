77471f24b7b810f74a7b210c616927a911072683
R.Armion <r.armion@tosan.com>
2023-01-14 17:12:08 +0330

    initializing project

---
+++ src/main/java/tosan/course/dockerchallenge/DockerChallengeApplication.java
@@ -0,0 +1,13 @@
+package tosan.course.dockerchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tosan/course/dockerchallenge/DockerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package tosan.course.dockerchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
