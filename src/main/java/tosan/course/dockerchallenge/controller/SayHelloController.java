8e931ca839d0f36e57b254a80004417024bc0362
R.Armion <r.armion@tosan.com>
2023-01-15 08:34:07 +0330

    added endpoint for /hello that returns 'Hello Stranger' closes #2

--- src/main/java/tosan/course/dockerchallenge/controller/SayHelloController.java
+++ src/main/java/tosan/course/dockerchallenge/controller/SayHelloController.java
@@ -1,7 +1,13 @@
 package tosan.course.dockerchallenge.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SayHelloController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello Stranger";
+    }
 }
